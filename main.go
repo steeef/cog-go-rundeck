4472e4638f63f3f7bf2879d19566ae88c8063fc9
Stephen Price <stephen@stp5.net>
2017-02-05 22:54:17 -0800

    list-jobs function

--- main.go
+++ main.go
@@ -4,19 +4,45 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"os"
+	"strconv"
 
 	rundeck "github.com/lusis/go-rundeck/src/rundeck.v17"
-	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
-var (
-	projectid = kingpin.Arg("projectid", "").Required().String()
-)
+func getProject() string {
+	optProject := os.Getenv("COG_OPT_PROJECT")
+	if len(optProject) == 0 {
+		defProject := os.Getenv("RUNDECK_DEFAULT_PROJECT")
+		if len(defProject) == 0 {
+			println("ERROR: Project not specified and RUNDECK_DEFAULT_PROJECT not set.")
+			os.Exit(1)
+		}
+		return defProject
+	}
+	return optProject
+}
 
-func main() {
-	kingpin.Parse()
+func getArgs() []string {
+	var args []string
+	argc, err := strconv.Atoi(os.Getenv("COG_ARGC"))
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	} else {
+		for i := 0; i < argc; i++ {
+			argName := "COG_ARGV_" + strconv.Itoa(i)
+			args = append(args, os.Getenv(argName))
+		}
+	}
+	return args
+}
+
+func listJobs() {
+	projectid := getProject()
+
 	client := rundeck.NewClientFromEnv()
-	data, err := client.ListJobs(*projectid)
+	data, err := client.ListJobs(projectid)
 	if err != nil {
 		fmt.Printf("%s\n", err)
 	} else {
@@ -28,5 +54,22 @@ func main() {
 			json.Indent(&prettyJSON, jsonData, "", "  ")
 			println(string(prettyJSON.Bytes()))
 		}
+	}
+
+}
+
+func main() {
+	args := getArgs()
+	if len(args) > 0 {
+		switch args[0] {
+		case "list-jobs":
+			listJobs()
+		default:
+			println("ERROR: unknown command.")
+			os.Exit(1)
+		}
+	} else {
+		println("ERROR: missing required arguments.")
+		os.Exit(1)
 	}
 }
